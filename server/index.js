c87003682813f61ab17e278d43f7386b0b20db54
DC-21 <cholahkuboko@gmail.com>
2023-06-28 12:20:13 +0200

    first commit

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ app.use(express.json());
 
 const db = require('./models');
 
+const helloRouter = require('./routes/helloRouter');
+
+app.use('/hello', helloRouter);
+
+
 db.sequelize.sync().then(()=>{
     app.listen(3000, ()=>{
         console.log("server is running on port 3000");
