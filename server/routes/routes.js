70e43a65b995f034e4c8cea8795d1dcfab39c23b
DC-21 <cholahkuboko@gmail.com>
2023-06-29 09:31:17 +0200

    first commit

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
-router.post('/user', user);
+router.post('/user', (req, res) => {
+    // Access the posted JSON data
+    const userData = req.body;
+  
+    // Perform any necessary operations with the user data
+  
+    console.log(userData); // Output the user data to the console
+  
+    // Send a response
+    res.status(200).json({ message: 'User created successfully!' });
+  });
 
 
 module.exports= router;
