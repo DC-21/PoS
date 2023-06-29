caab917b1a6157a2aa7f5a5cad9f55279fad6808
DC-21 <cholahkuboko@gmail.com>
2023-06-29 09:12:52 +0200

    first commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const sequelize=require('./utils/db')
+const sequelize = require('./utils/db');
 
 const app = express();
-
 
 // Configure bodyParser
 app.use(express.json());
@@ -11,6 +10,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.log(err);
+  const status = err.statusCode || 500;
+  const message = err.message;
+  res.status(status).json({ message: message });
+});
+
+// Define a GET request handler for the root endpoint
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
 // Test the database connection
 (async () => {
