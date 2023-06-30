26a6f67b916ddf46b1a34e6c67a8f56a2f88ddce
DC-21 <cholahkuboko@gmail.com>
2023-06-30 09:18:19 +0200

    first commit

--- client/src/components/Trans.jsx
+++ client/src/components/Trans.jsx
@@ -18,7 +18,44 @@ const Trans = () => {
   return (
     <div>
       {userDetails.map((value, key) => {
-        return <div key={key}>{value.accounttype}</div>;
+        return (
+          <div key={key} className="px-8 py-8">
+            <form className="gap-4 w-full h-screen items-center justify-center text-center">
+              <div className="w-full flex">
+                <label htmlFor="description1" className="w-1/2 text-start">
+                  Received from Account Type:
+                </label>
+                <input
+                  type="text"
+                  id="description1"
+                  placeholder="Customer"
+                  value={value.accounttype}
+                  className="w-1/2 bg-slate-100 border border-gray-400"
+                />
+              </div>
+              <div className="w-full flex">
+                <label htmlFor="description2" className="w-1/2 text-start">
+                  Received from Account No:
+                </label>
+                <input
+                  type="text"
+                  id="description2"
+                  placeholder="Customer"
+                  value={value.accountno}
+                  className="w-1/2 bg-slate-100 border border-gray-400"
+                />
+              </div>
+              <div>{value.accountname}</div>
+              <div>{value.accountbalance}</div>
+              <div>{value.amounttopay}</div>
+              <div>{value.amounttendered}</div>
+              <div>{value.change}</div>
+              <div>{value.paymenttype}</div>
+              <div>{value.description}</div>
+              <div>{value.incomegroupcode}</div>
+            </form>
+          </div>
+        );
       })}
     </div>
   );
