80dd9b9e9866ea82f41fb47f1de98e5486136c9a
DC-21 <cholahkuboko@gmail.com>
2023-07-09 09:03:58 +0200

    first commit

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,3 +1,13 @@
-const {Sequelize}= require('sequelize')
-const sequelize = new Sequelize("postgres://default:gG6RdhBjcl5M@ep-round-sea-185823-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb")
-module.exports=sequelize;
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize("postgres://default:gG6RdhBjcl5M@ep-round-sea-185823-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb", {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
+
+module.exports = sequelize;
