a26a5a1d21cc45ce659c2eddded959b37b9ea181
DC-21 <cholahkuboko@gmail.com>
2023-07-09 09:00:11 +0200

    first commit

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,6 +1,3 @@
 const {Sequelize}= require('sequelize')
-const sequelize = new Sequelize('businessDB', 'Cholah', 'Cholah@2104', {
-    host: 'localhost',
-    dialect: 'mysql',
-  });
+const sequelize = new Sequelize("postgres://default:gG6RdhBjcl5M@ep-round-sea-185823-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb")
 module.exports=sequelize;
