036210f38bb6fd47dec1bf9af7f4edaafba31e6c
DC-21 <cholahkuboko@gmail.com>
2023-07-31 09:44:16 +0200

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+const Navbar = () => {
+  return (
+    <div className='w-full py-2 px-4 sticky top-0 flex justify-center'>
+        <div className='px-4 py-6 bg-slate-400 rounded-md w-full flex justify-between'>
+            <a>Logo</a>
+            <a>Transactions</a>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import Trans from '../components/Trans'
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
         <Trans/>
     </div>
   )
