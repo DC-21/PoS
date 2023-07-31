3d33fb6d65f40eb566c23f32d30320cf2afa9802
DC-21 <cholahkuboko@gmail.com>
2023-07-31 09:47:49 +0200

    first commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-
+import logo from '../images/mulonga.png'
 const Navbar = () => {
   return (
     <div className='w-full py-2 px-4 sticky top-0 flex justify-center'>
-        <div className='px-4 py-6 bg-slate-400 rounded-md w-full flex justify-between'>
-            <a>Logo</a>
+        <div className='px-4 py-6 bg-slate-400 rounded-md w-full flex items-center justify-between'>
+            <a>
+                <img className='w-[200px] h-[80px] rounded' src={logo} alt='logo'/>
+            </a>
             <a>Transactions</a>
         </div>
     </div>
