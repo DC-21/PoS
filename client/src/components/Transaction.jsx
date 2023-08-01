a0731559c9c4471cc0d112dec099cb1d57da1d91
DC-21 <cholahkuboko@gmail.com>
2023-08-01 08:48:55 +0200

    first commit

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -23,22 +23,60 @@ const Transaction = () => {
     <div className="w-full py-2 px-4 flex justify-center">
       {transactions.length > 0 ? (
         <div className="px-4 py-2 bg-slate-200 rounded-md w-full h-auto pb-6 pt-6 flex items-center justify-center">
-          <ul className="flex flex-col gap-6 w-[600px] justify-center">
+          <ul className="flex flex-col gap-6 w-[400px] justify-center">
             {transactions.map((transaction) => (
               <li key={transaction.id}>
-                <div className="w-full flex">
-                <p className="text-center">Customer No: {transaction.customer_no}</p>
+                <div className="w-full flex justify-between">
+                  <p className="text-center">
+                    Receipt No:
+                  </p>
+                  <p>{transaction.rcptno}</p>
                 </div>
-                <p className="text-center">Receipt No: {transaction.rcptno}</p>
-                <p className="text-center">Customer No: {transaction.customer_no}</p>
+                <div className="w-full flex justify-between">
+                  <p className="text-center">
+                    Customer No:
+                  </p>
+                  <p>{transaction.customer_no}</p>
+                </div>
+                <div className="w-full flex justify-between">
+                  <p className="text-center">
+                    Customer Name:
+                  </p>
+                  <p>{transaction.name}</p>
+                </div>
+                <div className="w-full flex justify-between">
+                  <p className="text-center">
+                    Opening Balance:
+                  </p>
+                  <p>K{transaction.opn_bal}</p>
+                </div>
+                <div className="w-full flex justify-between">
+                  <p className="text-center">
+                    Closing Balance:
+                  </p>
+                  <p>K{transaction.clsn_bal}</p>
+                </div>
+                <p className="text-center">
+                  Customer No: {transaction.customer_no}
+                </p>
                 <p className="text-center">Name: {transaction.name}</p>
-                <p className="text-center">Opening Balance:{transaction.opn_bal}</p>
+                <p className="text-center">
+                  Opening Balance:{transaction.opn_bal}
+                </p>
                 <p className="text-center">Amount Paid:{transaction.amount}</p>
-                <p className="text-center">Amount Tendered:{transaction.amt_tnd}</p>
+                <p className="text-center">
+                  Amount Tendered:{transaction.amt_tnd}
+                </p>
                 <p className="text-center">Change:{transaction.change}</p>
-                <p className="text-center">Closing Balance:{transaction.clsn_bal}</p>
-                <p className="text-center">Payment Type: {transaction.pymt_type}</p>
-                <p className="text-center">Income Group Code:{transaction.code}</p>
+                <p className="text-center">
+                  Closing Balance:{transaction.clsn_bal}
+                </p>
+                <p className="text-center">
+                  Payment Type: {transaction.pymt_type}
+                </p>
+                <p className="text-center">
+                  Income Group Code:{transaction.code}
+                </p>
                 <p className="text-center">Description: {transaction.desc}</p>
               </li>
             ))}
