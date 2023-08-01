3e9bf66888ca4a079b621f305befde0d1fd6608a
DC-21 <cholahkuboko@gmail.com>
2023-08-01 09:06:33 +0200

    first commit

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -23,58 +23,58 @@ const Transaction = () => {
     <div className="w-full py-2 px-4 flex justify-center">
       {transactions.length > 0 ? (
         <div className="px-4 py-2 bg-slate-200 rounded-md w-full h-auto pb-6 pt-6 flex items-center justify-center">
-          <ul className="flex flex-col gap-6 w-[400px] justify-center">
+          <ul className="flex flex-col mt-6 gap-6 w-[450px] justify-center bg-white rounded">
             {transactions.map((transaction) => (
               <li key={transaction.id}>
-                <div className="w-full flex justify-between">
+                <div className="w-full flex justify-between px-4">
                   <p className="text-start flex-1">Transaction ID:</p>
                   <p className="text-start flex-1">{transaction.id}</p>
                 </div>
-                <div className="w-full flex justify-between">
+                <div className="w-full flex justify-between px-4">
                   <p className="text-start flex-1">Receipt No:</p>
                   <p className="text-start flex-1">{transaction.rcptno}</p>
                 </div>
-                <div className="w-full flex justify-between">
+                <div className="w-full flex justify-between px-4">
                   <p className="text-start flex-1">Date:</p>
                   <p className="text-start flex-1">{transaction.date}</p>
                 </div>
-                <div className="w-full flex justify-between">
+                <div className="w-full flex justify-between px-4">
                   <p className="text-start flex-1">Customer No:</p>
                   <p className="text-start flex-1">{transaction.customer_no}</p>
                 </div>
-                <div className="w-full flex justify-between">
+                <div className="w-full flex justify-between px-4">
                   <p className="text-start flex-1">Customer Name:</p>
                   <p className="text-start flex-1">{transaction.name}</p>
                 </div>
-                <div className="w-full flex justify-between">
+                <div className="w-full flex justify-between px-4">
                   <p className="text-start flex-1">Opening Balance:</p>
                   <p className="text-start flex-1">K{transaction.opn_bal}</p>
                 </div>
-                <div className="w-full flex justify-between">
+                <div className="w-full flex justify-between px-4">
                   <p className="text-start flex-1">Amount:</p>
                   <p className="text-start flex-1">K{transaction.amount}</p>
                 </div>
-                <div className="w-full flex justify-between">
+                <div className="w-full flex justify-between px-4">
                   <p className="text-start flex-1">Amount Tendered</p>
                   <p className="text-start flex-1">K{transaction.amt_tnd}</p>
                 </div>
-                <div className="w-full flex justify-between">
+                <div className="w-full flex justify-between px-4">
                   <p className="text-start flex-1">Change</p>
                   <p className="text-start flex-1">K{transaction.change}</p>
                 </div>
-                <div className="w-full flex justify-between">
+                <div className="w-full flex justify-between px-4">
                   <p className="text-start flex-1">Closing Balance:</p>
                   <p className="text-start flex-1">K{transaction.clsn_bal}</p>
                 </div>
-                <div className="w-full flex justify-between">
+                <div className="w-full flex justify-between px-4">
                   <p className="text-start flex-1">Description</p>
                   <p className="text-start flex-1">{transaction.desc}</p>
                 </div>
-                <div className="w-full flex justify-between">
+                <div className="w-full flex justify-between px-4">
                   <p className="text-start flex-1">Payment Type</p>
                   <p className="text-start flex-1">{transaction.pymt_type}</p>
                 </div>
-                <div className="w-full flex justify-between">
+                <div className="w-full flex justify-between px-4">
                   <p className="text-start flex-1">Income Group Code</p>
                   <p className="text-start flex-1">{transaction.code}</p>
                 </div>
