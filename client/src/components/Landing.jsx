22daea2ba907ce99fef3c1854e97c71f8b234436
DC-21 <cholahkuboko@gmail.com>
2023-08-03 10:08:49 +0200

    first commit

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -2,8 +2,14 @@
 const Landing = () => {
   return (
     <div className="flex w-full h-screen justify-center items-center">
-        <div className="flex w-full justify-center">
+        <div className="flex flex-col w-full justify-center items-center">
             <p className="text-blue-900 text-2xl">Welcome back</p>
+            <div className="mt-6">
+                <div className="justify-center gap-8 w-full flex">
+                    <button className="px-6 py-4 bg-blue-900 rounded text-white">New Transaction</button>
+                    <button>All Transactions</button>
+                </div>
+            </div>
         </div>
     </div>
   )
