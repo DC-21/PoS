ea8d9c185a02648e496e8efce3bd13335cc17fde
DC-21 <cholahkuboko@gmail.com>
2023-08-03 12:32:10 +0200

    first commit

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,29 +1,31 @@
-
 const TransactionsTable = () => {
   return (
-    <div>
-      <h2>Transaction History</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Amount</th>
-            <th>Amount</th>
-            <th>Amount</th>
-            <th>Amount</th>
-            <th>Amount</th>
-            <th>Amount</th>
-            <th>Amount</th>
-            <th>Amount</th>
-            <th>Amount</th>
-            <th>Column12</th>
-            <th>Column13</th>
-          </tr>
-        </thead>
-        <tbody>
-        </tbody>
-      </table>
+    <div className="mt-4">
+      <h2 className="text-lg font-semibold mb-2">Transaction History</h2>
+      <div className="overflow-x-auto">
+        <table className="min-w-full table-auto border-collapse border border-gray-300">
+          <thead>
+            <tr className="bg-gray-200">
+              <th className="border border-gray-300 px-4 py-2">
+                Transaction ID
+              </th>
+              <th className="border border-gray-300 px-4 py-2">Receipt No</th>
+              <th className="border border-gray-300 px-4 py-2">Date</th>
+              <th className="border border-gray-300 px-4 py-2">Customer No</th>
+              <th className="border border-gray-300 px-4 py-2">Name</th>
+              <th className="border border-gray-300 px-4 py-2">Opening Balance</th>
+              <th className="border border-gray-300 px-4 py-2">Amount</th>
+              <th className="border border-gray-300 px-4 py-2">Amount Tendered</th>
+              <th className="border border-gray-300 px-4 py-2">Change</th>
+              <th className="border border-gray-300 px-4 py-2">Closing Balance</th>
+              <th className="border border-gray-300 px-4 py-2">Description</th>
+              <th className="border border-gray-300 px-4 py-2">Payment Type</th>
+              <th className="border border-gray-300 px-4 py-2">Code</th>
+            </tr>
+          </thead>
+          <tbody>{/* ... table rows ... */}</tbody>
+        </table>
+      </div>
     </div>
   );
 };
