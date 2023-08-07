db2127931236779acd189840eba8323c6e4782fa
DC-21 <cholahkuboko@gmail.com>
2023-08-07 10:11:56 +0200

    first commit

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -6,25 +6,25 @@ const Profile = () => {
       <div className="w-full flex flex-col px-4 items-center">
         <p className="text-xl">Profiles Settings</p>
         <div className="w-full flex items-center px-4 py-6 rounded justify-between bg-[#27105e]">
-          <div className="rounded-full overflow-hidden">
+          <div className="rounded-full overflow-hidden flex">
             <img className="w-[150px] h-[150px]" src={own} alt="own" />
           </div>
-          <div>
-          <div className="flex gap-4">
+          <div className="bg-white h-[200px] rounded flex flex-col justify-center">
+            <div className="flex gap-4">
               <p className="text-white">Name:</p>
               <p className="text-white">Chola Kuboko</p>
             </div>
             <div className="flex gap-4">
               <p className="text-white">Username:</p>
-              <p className="text-white">Chola Kuboko</p>
+              <p className="text-white">Cholah</p>
             </div>
             <div className="flex gap-4">
-              <p className="text-white">Name:</p>
-              <p className="text-white">Chola Kuboko</p>
+              <p className="text-white">Contact:</p>
+              <p className="text-white">0979054417</p>
             </div>
             <div className="flex gap-4">
-              <p className="text-white">Name:</p>
-              <p className="text-white">Chola Kuboko</p>
+              <p className="text-white">Email:</p>
+              <p className="text-white">cholahKuboko@gmail.com</p>
             </div>
           </div>
           <div>
