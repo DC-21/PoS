429e2d674cecbe4031dd0044a2c195a44f332263
DC-21 <cholahkuboko@gmail.com>
2023-08-07 17:57:55 +0200

    first commit

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -80,49 +80,49 @@ const Profile = () => {
       <div className="w-full flex flex-col px-4 items-center pt-[120px]">
         <p className="text-xl">Profiles Settings</p>
         <div className="w-full flex items-center px-4 py-6 mt-4 rounded justify-between bg-[#27105e]">
-          <div className="bg-white h-[200px] w-[200px] justify-center rounded items-center overflow-hidden flex">
+          <div className="bg-[#391886] h-[200px] w-[200px] justify-center rounded items-center overflow-hidden flex">
             <img
               className="w-[180px] h-[180px] rounded-full"
               src={own}
               alt="own"
             />
           </div>
-          <div className="bg-white w-[450px] h-[200px] rounded flex flex-col items-center justify-center">
+          <div className="bg-[#391886] w-[450px] h-[200px] rounded flex flex-col items-center justify-center">
             <div className="flex gap-4 w-full py-2 px-4">
-              <p className="flex-1 text-start text-black">Name:</p>
-              <p className="flex-1 text-start text-black">Chola Kuboko</p>
+              <p className="flex-1 text-start text-white">Name:</p>
+              <p className="flex-1 text-start text-white">Chola Kuboko</p>
             </div>
             <div className="flex gap-4 w-full py-2 px-4">
-              <p className="flex-1 text-start text-black">Username:</p>
-              <p className="flex-1 text-start text-black">Cholah</p>
+              <p className="flex-1 text-start text-white">Username:</p>
+              <p className="flex-1 text-start text-white">Cholah</p>
             </div>
             <div className="flex gap-4 w-full py-2 px-4">
-              <p className="flex-1 text-start text-black">Contact:</p>
-              <p className="flex-1 text-start text-black">0979054417</p>
+              <p className="flex-1 text-start text-white">Contact:</p>
+              <p className="flex-1 text-start text-white">0979054417</p>
             </div>
             <div className="flex gap-4 w-full py-2 px-4">
-              <p className="flex-1 text-start text-black">Email:</p>
-              <p className="flex-1 text-start text-black">
+              <p className="flex-1 text-start text-white">Email:</p>
+              <p className="flex-1 text-start text-white">
                 cholahKuboko@gmail.com
               </p>
             </div>
           </div>
-          <div className="bg-white w-[450px] h-[200px] rounded flex flex-col items-center justify-center">
+          <div className="bg-[#391886] w-[450px] h-[200px] rounded flex flex-col items-center justify-center">
             <div className="flex gap-4 w-full py-2 px-4">
-              <p className="flex-1 text-start text-black">Role:</p>
-              <p className="flex-1 text-start text-black">Admin</p>
+              <p className="flex-1 text-start text-white">Role:</p>
+              <p className="flex-1 text-start text-white">Admin</p>
             </div>
             <div className="flex gap-4 w-full py-2 px-4">
-              <p className="flex-1 text-start text-black">Profession:</p>
-              <p className="flex-1 text-start text-black">Software Engineer</p>
+              <p className="flex-1 text-start text-white">Profession:</p>
+              <p className="flex-1 text-start text-white">Software Engineer</p>
             </div>
             <div className="flex gap-4 w-full py-2 px-4">
-              <p className="flex-1 text-start text-black">Address:</p>
-              <p className="flex-1 text-start text-black">9 miles</p>
+              <p className="flex-1 text-start text-white">Address:</p>
+              <p className="flex-1 text-start text-white">9 miles</p>
             </div>
             <div className="flex gap-4 w-full py-2 px-4">
-              <p className="flex-1 text-start text-black">Address 2:</p>
-              <p className="flex-1 text-start text-black">Minestone RD.</p>
+              <p className="flex-1 text-start text-white">Address 2:</p>
+              <p className="flex-1 text-start text-white">Minestone RD.</p>
             </div>
           </div>
         </div>
