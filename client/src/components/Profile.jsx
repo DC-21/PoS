ff4503043c789a1eca508f278835cdefc084d1fb
DC-21 <cholahkuboko@gmail.com>
2023-08-07 09:41:41 +0200

    first commit

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,8 +1,22 @@
+import own from "../images/own.png";
 
 const Profile = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="w-full h-screen justify-center items-center flex">
+      <div className="w-full flex flex-col px-4 items-center">
+        <p className="text-xl">Profiles Settings</p>
+        <div className="w-full flex items-center mt-4 py-6 rounded bg-[#27105e]">
+          <div className="rounded-full bg-white overflow-hidden">
+            <img
+              className="w-[150px] h-[150px]"
+              src={own}
+              alt="own"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
