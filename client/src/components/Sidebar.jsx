9393626d8ca0900a936cd5f7417fd13ddb7257d4
DC-21 <cholahkuboko@gmail.com>
2023-08-07 11:16:31 +0200

    first commit

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,16 +1,16 @@
 const Sidebar = () => {
   return (
-    <div className="w-[100px] h-[470px] mt-4 rounded justify-center flex flex-col bg-[#27105e] sticky left-2">
-      <div className="w-full flex justify-start py-2 px-3 items-center gap-2">
+    <div className="w-[100px] h-[470px] mt-4 rounded py-4 flex flex-col bg-[#27105e] sticky left-2">
+      <div className="w-full flex justify-start py-2 px-3 items-center gap-2 hover:bg-[#6e617e]">
         <p className="text-white">Profile</p>
       </div>
-      <div className="w-full flex justify-start py-2 px-3 items-center gap-2">
+      <div className="w-full flex justify-start py-2 px-3 items-center gap-2 hover:bg-[#6e617e]">
         <p className="text-white">Password</p>
       </div>
-      <div className="w-full flex justify-start py-2 px-3 items-center gap-2">
+      <div className="w-full flex justify-start py-2 px-3 items-center gap-2 hover:bg-[#6e617e]">
         <p className="text-white">Team</p>
       </div>
-      <div className="w-full flex justify-start py-2 px-3 items-center gap-2">
+      <div className="w-full flex justify-start py-2 px-3 items-center gap-2 hover:bg-[#6e617e]">
         <p className="text-white">Add User</p>
       </div>
     </div>
