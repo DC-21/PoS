2c67f0a1678a61d6295cc45595c7020e126d20ff
DC-21 <cholahkuboko@gmail.com>
2023-08-08 10:25:36 +0200

    first commit

--- client/src/components/OpenReceipts.jsx
+++ client/src/components/OpenReceipts.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import ReactHTMLTableToExcel from "react-html-table-to-excel";
 import useTransactionStore from "../Store";
-import Receipt from "./Receipt";
 
 const TransactionsTable = () => {
   const transactions = useTransactionStore((state) => state.transactions);
@@ -166,7 +165,6 @@ const TransactionsTable = () => {
                     <td className="border border-gray-300 px-4 py-2">
                       {transaction.code}
                     </td>
-                    <td>Receipt</td>
                     <td>
                       <input
                         type="checkbox"
