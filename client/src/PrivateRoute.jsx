75be8abae3a0202f27299020809329a9244da8e2
DC-21 <cholahkuboko@gmail.com>
2023-08-09 16:32:42 +0200

    first commit

--- client/src/PrivateRoute.jsx
+++ client/src/PrivateRoute.jsx
@@ -1,6 +1,2 @@
 import { Route, Navigate } from "react-router-dom";
 
-const PrivateRoute = ({ element, isAuthenticated, ...rest }) => {
-  if (isAuthenticated) {
-    return <Route element={element} {...rest} />;
-  } else {
