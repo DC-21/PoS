26a0c271d64260cd1016d652a6fbcd20e228ac0e
DC-21 <cholahkuboko@gmail.com>
2023-08-09 16:32:21 +0200

    first commit

--- client/src/PrivateRoute.jsx
+++ client/src/PrivateRoute.jsx
@@ -4,14 +4,3 @@ const PrivateRoute = ({ element, isAuthenticated, ...rest }) => {
   if (isAuthenticated) {
     return <Route element={element} {...rest} />;
   } else {
-    // Redirect to the login screen if not authenticated
-    return <Navigate to="/login" />;
-  }
-};
-
-PrivateRoute.propTypes = {
-  element: PropTypes.element.isRequired,
-  isAuthenticated: PropTypes.bool.isRequired,
-};
-
-export default PrivateRou
