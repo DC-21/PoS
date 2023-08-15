cffb9ee9f91fad5c908b7c85ac3eebfb38bc54cd
DC-21 <cholahkuboko@gmail.com>
2023-08-15 10:12:15 +0200

    first commit

--- client/src/components/Trans.jsx
+++ client/src/components/Trans.jsx
@@ -83,12 +83,16 @@ const Trans = () => {
     handleClosePopup();
   };
 
-
-  const filteredOptions = selectedAccountType === "customer"
-    ? userDetails.filter(user => user.name.toLowerCase().includes(searchText.toLowerCase()))
-    : selectedAccountType === "accounts"
-    ? glaccounts.filter(glaccount => glaccount.name.toLowerCase().includes(searchText.toLowerCase()))
-    : [];
+  const filteredOptions =
+    selectedAccountType === "customer"
+      ? userDetails.filter((user) =>
+          user.name.toLowerCase().includes(searchText.toLowerCase())
+        )
+      : selectedAccountType === "accounts"
+      ? glaccounts.filter((glaccount) =>
+          glaccount.name.toLowerCase().includes(searchText.toLowerCase())
+        )
+      : [];
 
   //this section makes a request to find customer details based on the selected name. //
   useEffect(() => {
@@ -138,7 +142,9 @@ const Trans = () => {
     setChange("00.00");
   };
 
-  const formattedDateForDB = moment(currentDate, "DD/MM/YY").format("YYYY-MM-DD");
+  const formattedDateForDB = moment(currentDate, "DD/MM/YY").format(
+    "YYYY-MM-DD"
+  );
   const handleSubmit = () => {
     const newBalanceDueLCY =
       parseFloat(balanceDueLCY) - parseFloat(amountToPay);
@@ -253,26 +259,37 @@ const Trans = () => {
           {selectedAccountName || "Select An Account Name To Receive From"}
         </div>
         {showPopup && (
-          <div className="popup" style={{ maxHeight: '200px', overflowY: 'auto', backgroundColor:"gray" }}>
-            <input
-              type="text"
-              placeholder="Search Account Name"
-              className="w-full bg-slate-100 border border-gray-400"
-              value={searchText}
-              onChange={(e) => setSearchText(e.target.value)}
-            />
-            <div className="search-results">
-              {filteredOptions.map(option => (
-                <div
-                  key={option.id || option.code}
-                  className="result-item"
-                  onClick={() => handleSelectAccount(option.name)}
-                >
-                  {option.name}
+          <div className="popup-overlay fixed inset-0 z-10 flex justify-center items-center">
+            <div className="popup bg-white w-64 p-4 border rounded shadow">
+              <input
+                type="text"
+                placeholder="Search Account Name"
+                className="w-full bg-gray-100 border border-gray-400 p-2 rounded mb-2"
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+              />
+              {filteredOptions.length > 0 ? (
+                <div className="search-results max-h-40 overflow-y-auto">
+                  {filteredOptions.map(option => (
+                    <div
+                      key={option.id || option.code}
+                      className="result-item cursor-pointer px-2 py-1 hover:bg-gray-200 rounded"
+                      onClick={() => handleSelectAccount(option.name)}
+                    >
+                      {option.name}
+                    </div>
+                  ))}
                 </div>
-              ))}
+              ) : (
+                <div className="text-center mt-2 text-gray-500">No results found.</div>
+              )}
+              <button
+                className="mt-2 bg-gray-300 hover:bg-gray-400 px-2 py-1 rounded"
+                onClick={handleClosePopup}
+              >
+                Close
+              </button>
             </div>
-            <button onClick={handleClosePopup}>Close</button>
           </div>
         )}
       </div>
