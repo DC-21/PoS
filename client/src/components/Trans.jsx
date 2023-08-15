6b7fec71567758fa0bc1b96cf24d2a381b58e166
DC-21 <cholahkuboko@gmail.com>
2023-08-15 10:31:30 +0200

    first commit

--- client/src/components/Trans.jsx
+++ client/src/components/Trans.jsx
@@ -94,13 +94,13 @@ const Trans = () => {
     };
 
     if (showPopup) {
-      document.addEventListener('mousedown', handleOutsideClick);
+      document.addEventListener("mousedown", handleOutsideClick);
     } else {
-      document.removeEventListener('mousedown', handleOutsideClick);
+      document.removeEventListener("mousedown", handleOutsideClick);
     }
 
     return () => {
-      document.removeEventListener('mousedown', handleOutsideClick);
+      document.removeEventListener("mousedown", handleOutsideClick);
     };
   }, [showPopup]);
 
@@ -269,27 +269,31 @@ const Trans = () => {
             </select>
           </div>
           <div className="w-full flex">
-      <label htmlFor="description3" className="w-1/2 text-start">
-        Received from Account Name:
-      </label>
-      <div className="w-1/2 bg-slate-100 border border-gray-400 relative">
-        <div
-          className="w-full h-full cursor-pointer"
-          onClick={handleOpenPopup}
-        >
-          {selectedAccountName || "Select An Account Name To Receive From"}
-        </div>
-        {showPopup && (
-          <div className="popup-overlay fixed inset-0 z-10 flex justify-center items-center">
-            <div className="popup bg-white w-64 p-4 border rounded shadow" ref={popupRef}>
-              <input
-                type="text"
-                placeholder="Search Account Name"
-                className="w-full bg-gray-100 border border-gray-400 p-2 rounded mb-2"
-                value={searchText}
-                onChange={(e) => setSearchText(e.target.value)}
-              />
-              {filteredOptions.length > 0 ? (
+            <label htmlFor="description3" className="w-1/2 text-start">
+              Received from Account Name:
+            </label>
+            <div className="w-1/2 text-start bg-slate-100 border border-gray-400 relative">
+              <div
+                className="w-full h-full cursor-pointer"
+                onClick={handleOpenPopup}
+              >
+                {selectedAccountName ||
+                  "Select An Account Name To Receive From"}
+              </div>
+              {showPopup && (
+                <div className="popup-overlay fixed inset-0 z-10 flex justify-center items-center">
+                  <div
+                    className="popup bg-white w-64 p-4 border rounded shadow"
+                    ref={popupRef}
+                  >
+                    <input
+                      type="text"
+                      placeholder="Search Account Name"
+                      className="w-full bg-gray-100 border border-gray-400 p-2 rounded mb-2"
+                      value={searchText}
+                      onChange={(e) => setSearchText(e.target.value)}
+                    />
+                    {filteredOptions.length > 0 ? (
                 <div className="search-results max-h-40 overflow-y-auto">
                   {filteredOptions.map(option => (
                     <div
@@ -297,24 +301,27 @@ const Trans = () => {
                       className="result-item cursor-pointer px-2 py-1 hover:bg-gray-200 rounded"
                       onClick={() => handleSelectAccount(option.name)}
                     >
-                      {option.name}
+                      <span className="font-semibold">{option.name}</span>
+                      <span className="text-gray-500 ml-1">({option.customerNo})</span>
                     </div>
                   ))}
                 </div>
               ) : (
-                <div className="text-center mt-2 text-gray-500">No results found.</div>
+                      <div className="text-center mt-2 text-gray-500">
+                        No results found.
+                      </div>
+                    )}
+                    <button
+                      className="mt-2 bg-gray-300 hover:bg-gray-400 px-2 py-1 rounded"
+                      onClick={handleClosePopup}
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
               )}
-              <button
-                className="mt-2 bg-gray-300 hover:bg-gray-400 px-2 py-1 rounded"
-                onClick={handleClosePopup}
-              >
-                Close
-              </button>
             </div>
           </div>
-        )}
-      </div>
-    </div>
 
           <div className="w-full flex">
             <label htmlFor="description2" className="w-1/2 text-start">
