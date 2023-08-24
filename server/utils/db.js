6da4e8796d3f318b873641d14b48e6311baa8a31
DC-21 <cholahkuboko@gmail.com>
2023-08-24 21:55:21 +0200

    first commit

--- server/utils/db.js
+++
@@ -1,6 +0,0 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('businessDB', 'Cholah', 'Cholah@2104', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-module.exports = sequelize;
