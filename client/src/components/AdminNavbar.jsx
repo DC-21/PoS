79c7af47a2104f4559aca205dbe9673aec4efb7f
DC-21 <cholahkuboko@gmail.com>
2023-09-04 16:47:36 +0200

    first commit

--- client/src/components/AdminNavbar.jsx
+++ client/src/components/AdminNavbar.jsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import logo from "../images/mulonga.png";
 import useUserStore from '../Userstore';
 import axios from "axios";
 
 const Navbar = () => {
   const { userName, setUserName } = useUserStore();
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const token = sessionStorage.getItem("jwtToken");
@@ -22,16 +21,12 @@ const Navbar = () => {
           const fullName = userData.full_name;
 
           setUserName(fullName);
-          setLoading(false);
         })
         .catch((error) => {
           console.error("Error fetching user data:", error);
-          setLoading(false);
         });
     }
   }, []);
-  console.log("Loading:", loading);
-  console.log("UserName:", userName);
 
   return (
     <div className="w-full sticky top-0 flex justify-center">
@@ -60,11 +55,7 @@ const Navbar = () => {
             <path d="M12 2A10.13 10.13 0 002 12a10 10 0 004 7.92V20h.1a9.7 9.7 0 0011.8 0h.1v-.08A10 10 0 0022 12 10.13 10.13 0 0012 2zM8.07 18.93A3 3 0 0111 16.57h2a3 3 0 012.93 2.36 7.75 7.75 0 01-7.86 0zm9.54-1.29A5 5 0 0013 14.57h-2a5 5 0 00-4.61 3.07A8 8 0 014 12a8.1 8.1 0 018-8 8.1 8.1 0 018 8 8 8 0 01-2.39 5.64z" />
             <path d="M12 6a3.91 3.91 0 00-4 4 3.91 3.91 0 004 4 3.91 3.91 0 004-4 3.91 3.91 0 00-4-4zm0 6a1.91 1.91 0 01-2-2 1.91 1.91 0 012-2 1.91 1.91 0 012 2 1.91 1.91 0 01-2 2z" />
           </svg>
-          {loading ? (
-            <span className="text-white">Loading...</span>
-          ) : (
             <span className="text-white">{userName}</span>
-          )}
         </Link>
       </div>
     </div>
